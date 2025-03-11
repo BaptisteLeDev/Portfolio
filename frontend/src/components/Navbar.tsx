be6bac2877958265e90c96d7d66a200d9fa330b6
BaptisteD <baptiste.dechamp@outlook.fr>
2025-03-11 10:56:44 +0100

    Test CV pdf

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -32,23 +32,31 @@ const Navbar = () => {
       {/* Logo */}
       
       <div>
-  <Link
-    to="/"
-    className="relative flex items-center justify-center w-16 h-16 rounded-full bg-white 
-               transition-all duration-300 hover:scale-110 hover:shadow-lg
-               "
-  >
-    <img
-      src={Logo}
-      alt="Logo"
-      className="w-10 h-10 drop-shadow-[0_0_10px_rgba(255,255,255,0.15)]"
-    />
-  </Link>
-</div>
-
+        <Link
+          to="/"
+          className="relative flex items-center justify-center w-16 h-16 rounded-full bg-white 
+                     transition-all duration-300 hover:scale-110 hover:shadow-lg"
+        >
+          <img
+            src={Logo}
+            alt="Logo"
+            className="w-10 h-10 drop-shadow-[0_0_10px_rgba(255,255,255,0.15)]"
+          />
+        </Link>
+      </div>
 
       <div className="space-x-6 flex left-25 me-10">
-        <NavLink to="/">CV</NavLink>
+        <a
+          href="/TheCV_Baptiste_DECHAMP.pdf"
+          download
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative text-white text-sm md:text-base font-medium transition duration-300 
+                     hover:text-gray-300 after:block after:h-[2px] after:bg-white 
+                     after:w-0 hover:after:w-full after:transition-all after:duration-300"
+        >
+          CV
+        </a>
         <NavLink to="/portfolio">Portfolio</NavLink>
         <NavLink to="/bonus">Contact</NavLink>
       </div>
